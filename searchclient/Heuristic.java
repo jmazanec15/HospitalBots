67447ac6112773e0a439befde86123f4b033756e
Jack Mazanec <jmazane1@nd.edu>
2018-04-06 14:58:41 +0200

    dij

--- searchclient/Heuristic.java
+++ searchclient/Heuristic.java
@@ -8,17 +8,20 @@ import java.util.ArrayList;
 
 import searchclient.NotImplementedException;
 
+
+// Used ~ using dijstras ~ ADAPTED FROM https://www.geeksforgeeks.org/greedy-algorithms-set-6-dijkstras-shortest-path-algorithm/ for implementation of DIJ
+
 public abstract class Heuristic implements Comparator<Node> {
 	private int distances[][][];
-	// private Vector<Int[]> goals;
 	private ArrayList<int[]> goals;
-
+	private int MR, MC;
 	public Heuristic(Node initialState) {
+		// Init vals
+		this.MR = initialState.MAX_ROW;
+		this.MC = initialState.MAX_COL;
 		/****** Preprocessing ******/
-		// Calculating the Manhatten distance for cells to goals
-		// 0.) Set list of goal states coords and values
+		// 1.) Set list of goal states coords and values
 		this.goals = new ArrayList<int[]>();
-		// Vector goalChars  = new Vector();
 		for (int row = 0; row < initialState.MAX_ROW; row++) {
 			for (int col = 0; col < initialState.MAX_COL; col++) {
 				if ((int)initialState.goals[row][col] > 0) {
@@ -26,19 +29,59 @@ public abstract class Heuristic implements Comparator<Node> {
 					this.goals.add(nP);
 				}
 			}
-		}		
-		// For each goal
-		this.distances  = new int [initialState.MAX_ROW][initialState.MAX_COL][this.goals.size()]; // not sure I can dynamically set this -- may have to put in the node thing
-		for (int row = 0; row < initialState.MAX_ROW; row++) {
-			for (int col = 0; col < initialState.MAX_COL; col++) {
-				// if the node is a wall, continue
-				if (initialState.walls[row][col]) {continue;}
-				// Loop through each goal and calculate the distance to the cell
-				for (int goal = 0; goal < this.goals.size(); goal++) {
-					this.distances[row][col][goal] = Math.abs(row - this.goals.get(goal)[0]) + Math.abs(col - this.goals.get(goal)[1]);
+		}
+		// 2.) For each goal, compute dijstras to all other nodes in the graph
+		this.distances  = new int [initialState.MAX_ROW][initialState.MAX_COL][this.goals.size()];
+		Boolean[][][] sptSet = new Boolean [initialState.MAX_ROW][initialState.MAX_COL][this.goals.size()]; // true if already in the path
+		int coords[] = new int [2];
+		int r, c;
+		// Init all distances as infinite
+		for (int goal = 0; goal < this.goals.size() ; goal++) {
+			for (int row = 0; row < initialState.MAX_ROW; row++) {
+				for (int col = 0; col < initialState.MAX_COL; col++) {
+					this.distances[row][col][goal] = Integer.MAX_VALUE;
+					sptSet[row][col][goal] = false;
+
+				}
+			}
+			// Set source node distance to 0
+			this.distances[this.goals.get(goal)[0]][this.goals.get(goal)[1]][goal] = 0;
+			// Find shortest path to all vertices
+			for (int i = 0; i < initialState.MAX_ROW*initialState.MAX_COL; i++) {
+				coords = this.minDistance(goal, sptSet);
+				r = coords[0];
+				c = coords[1];
+				sptSet[r][c][goal] = true;
+				// Check four neighbor cells to update
+				if (r > 0 && !initialState.walls[r-1][c] && !sptSet[r-1][c][goal] && this.distances[r][c][goal] != Integer.MAX_VALUE && this.distances[r][c][goal] + 1 < this.distances[r-1][c][goal]) {
+					this.distances[r-1][c][goal] = this.distances[r][c][goal] + 1;
+				}
+				if (r < initialState.MAX_ROW - 1 && !initialState.walls[r+1][c] && !sptSet[r+1][c][goal] && this.distances[r][c][goal] != Integer.MAX_VALUE && this.distances[r][c][goal] + 1 < this.distances[r+1][c][goal]) {
+					this.distances[r+1][c][goal] = this.distances[r][c][goal] + 1;
+				}
+				if (c > 0 && !initialState.walls[r][c-1] && !sptSet[r][c-1][goal] && this.distances[r][c][goal] != Integer.MAX_VALUE && this.distances[r][c][goal] + 1 < this.distances[r][c-1][goal]) {
+					this.distances[r][c-1][goal] = this.distances[r][c][goal] + 1;
+				}		
+				if (c < initialState.MAX_COL && !initialState.walls[r][c+1] && !sptSet[r][c+1][goal] && this.distances[r][c][goal] != Integer.MAX_VALUE && this.distances[r][c][goal] + 1 < this.distances[r][c+1][goal]) {
+					this.distances[r][c+1][goal] = this.distances[r][c][goal] + 1;
+				}				
+			}
+		}
+	}
+	// Calculate the next minimum node in the coords and return coords
+	private int[] minDistance(int goal, Boolean[][][] sptSet) {
+		int mini = Integer.MAX_VALUE;
+		int[] m_coords = {-1, -1};
+		for (int row = 0; row < this.MR; row++) {
+			for (int col = 0; col < this.MC; col++) {
+				if (/*sptSet[row][col][goal] == false &&*/ this.distances[row][col][goal] < mini) {
+					mini = this.distances[row][col][goal];
+					m_coords[0] = row;
+					m_coords[1] = col;
 				}
 			}
 		}
+		return m_coords;
 	}
 
 	public int h(Node n) {
@@ -56,20 +99,6 @@ public abstract class Heuristic implements Comparator<Node> {
 			}
 			h_val += min_dist;
 		}
-
-		// for (int row = 0; row < n.MAX_ROW; row++) {
-		// 	for (int col = 0; col < n.MAX_COL; col++) {
-		// 		if (!n.boxAt(row, col)) {continue;} // if there is not a box continue
-		// 		// If there is, determine which character it is
-		// 		// Then match it with a goal state
-		// 		for (int goal = 0; goal < this.goals.size(); goal++) {
-		// 			// if ((int)n.boxes[row][col] == this.goals.get(goal)[2]) {
-		// 			// 	// have to somehow minimize everything
-		// 			// 	h_val += this.distances[row][col][goal];
-		// 			// }
-		// 		}
-		// 	}
-		// }
 		return h_val;
 	}
 
